c7b671ff26927b7e1d952d0830ca9db2a026622a
Timofey Novitskiy <i.see.you911@gmail.com>
2016-08-16 12:50:54 +0300

    init

---
+++ spec/task-manager-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TaskManagerView from '../lib/task-manager-view';
+
+describe('TaskManagerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
